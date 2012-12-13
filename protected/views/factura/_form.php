643c3a31e8ef8544eb488f69ad50869b61c5d68f
Juan David Rodriguez Ramirez <juan.rodriguez@tramasoft.com>
2012-12-13 19:29:02 -0300

    Termincacion de calculos para crear
    se impleementaron los calculos distintos a los que tenia guillermo

--- protected/views/factura/_form.php
+++ protected/views/factura/_form.php
@@ -87,11 +87,12 @@ function cargaGrilla(grid_id){
              ),	
     )); 
     $conf = ConfFa::model()->find();
-    $rubro1 =$conf->USAR_RUBROS && $conf->RUBRO1_NOMBRE != '' ? $form->textFieldRow($model,'RUBRO1') : '';
-    $rubro2 =$conf->USAR_RUBROS && $conf->RUBRO2_NOMBRE != '' ? $form->textFieldRow($model,'RUBRO2') : '';
-    $rubro3 =$conf->USAR_RUBROS && $conf->RUBRO3_NOMBRE != '' ? $form->textFieldRow($model,'RUBRO3') : '';
-    $rubro4 =$conf->USAR_RUBROS && $conf->RUBRO4_NOMBRE != '' ? $form->textFieldRow($model,'RUBRO4') : '';
-    $rubro5 =$conf->USAR_RUBROS && $conf->RUBRO5_NOMBRE != '' ? $form->textFieldRow($model,'RUBRO5') : '';
+    $text_rubros =$conf->USAR_RUBROS == 0 ? '<div class="alert alert-info"><strong>Actualmente No usa Rubros</strong></div>' : '';
+    $rubro1 =$conf->USAR_RUBROS == 1 && $conf->RUBRO1_NOMBRE != '' ? $form->textFieldRow($model,'RUBRO1') : '';
+    $rubro2 =$conf->USAR_RUBROS == 1 && $conf->RUBRO2_NOMBRE != '' ? $form->textFieldRow($model,'RUBRO2') : '';
+    $rubro3 =$conf->USAR_RUBROS == 1 && $conf->RUBRO3_NOMBRE != '' ? $form->textFieldRow($model,'RUBRO3') : '';
+    $rubro4 =$conf->USAR_RUBROS == 1 && $conf->RUBRO4_NOMBRE != '' ? $form->textFieldRow($model,'RUBRO4') : '';
+    $rubro5 =$conf->USAR_RUBROS == 1 && $conf->RUBRO5_NOMBRE != '' ? $form->textFieldRow($model,'RUBRO5') : '';
 ?>
     
 <?php
@@ -135,6 +136,24 @@ function cargaGrilla(grid_id){
     
     $fechaEntrega = $this->widget('zii.widgets.jui.CJuiDatePicker', array(
         'attribute'=>'FECHA_ENTREGA',
+        'model'=>$model,
+	'language'=>'es',
+	'options'=>array(
+            'showAnim'=>'fadeIn', // 'show' (the default), 'slideDown', 'fadeIn', 'fold'
+            'dateFormat'=>'yy-mm-dd',
+            'changeMonth'=>true,
+            'changeYear'=>true,
+            'showOn'=>'both', // 'focus', 'button', 'both'
+            'buttonText'=>Yii::t('ui','Select form calendar'), 
+            'buttonImage'=>Yii::app()->request->baseUrl.'/images/calendar.gif', 
+            'buttonImageOnly'=>true,
+	),
+        'htmlOptions'=>array(
+            'style'=>'width:80px;vertical-align:top'
+        ),  
+   ), true); 
+    $fechaOrden = $this->widget('zii.widgets.jui.CJuiDatePicker', array(
+        'attribute'=>'FECHA_ORDEN',
         'model'=>$model,
 	'language'=>'es',
 	'options'=>array(
@@ -234,37 +253,67 @@ function cargaGrilla(grid_id){
                 'tabs'=>array( 
                     array('label'=>'Líneas', 'content'=>$renderLineas, 'active'=>true),
                     array('label'=>'Otros', 'content'=>
-                        '<div class="row">
-                            <div class="control-group ">'
-                                .$form->labelEx($model,'FECHA_DESPACHO',array('class'=>'control-label'))
-                                .'<div class="controls">'   
-                                .$fechaDespacho
-                                .'</div>
-                            </div>'
-                            .'<div class="control-group ">'
-                                .$form->labelEx($model,'FECHA_ENTREGA',array('class'=>'control-label'))
-                                .'<div class="controls">'   
-                                .$fechaEntrega
-                                .'</div>
-                            </div>'
-                        .'</div>'
-                        .$rubro1
-                        .$rubro2
-                        .$rubro3
-                        .$rubro4
-                        .$rubro5
-                        .$form->textFieldRow($model,'COMENTARIOS_CXC',array('size'=>50,'maxlength'=>50))
-                        .$form->textAreaRow($model,'OBSERVACIONES',array('rows'=>6, 'cols'=>50))
-                        ),
+                        '<table>
+                            <tr>
+                                <td style="width: 380px;">
+                                    <fieldset >
+                                        <legend ><font face="arial" size=3 >Fechas</font></legend>
+                                        <div class="control-group ">'
+                                            .$form->labelEx($model,'FECHA_DESPACHO',array('class'=>'control-label'))
+                                            .'<div class="controls">'   
+                                            .$fechaDespacho
+                                            .'</div>
+                                        </div>'
+                                        .'<div class="control-group ">'
+                                            .$form->labelEx($model,'FECHA_ENTREGA',array('class'=>'control-label'))
+                                            .'<div class="controls">'   
+                                            .$fechaEntrega
+                                            .'</div>
+                                        </div>'
+                                   .'</fieldset>'
+                                   .$form->textFieldRow($model,'COMENTARIOS_CXC',array('size'=>50,'maxlength'=>50))
+                                   .$form->textAreaRow($model,'OBSERVACIONES',array('rows'=>6, 'cols'=>50))
+                               .'</td>
+                                <td>
+                                    <fieldset>
+                                        <legend ><font face="arial" size=3 >Rubros</font></legend>'
+                                        .$text_rubros
+                                        .$rubro1
+                                        .$rubro2
+                                        .$rubro3
+                                        .$rubro4
+                                        .$rubro5
+                                    .'</fieldset>
+                               </td>
+                            </tr>
+                       </table>
+                       <fieldset>
+                               <legend ><font face="arial" size=3 >Orden de Compra</font></legend>
+                               <table>
+                                    <tr>
+                                        <td style="width: 380px;">'
+                                            .$form->textFieldRow($model,'ORDEN_COMPRA',array('size'=>30,'maxlength'=>50))
+                                         .'</td>
+                                        <td>
+                                            <div class="control-group ">'
+                                                .$form->labelEx($model,'FECHA_ORDEN',array('class'=>'control-label'))
+                                                .'<div class="controls">'   
+                                                    .$fechaOrden
+                                                .'</div>
+                                            </div>
+                                        </td>
+                                    <tr>
+                            </table>
+                      </fieldset>'
+                    ),
                     array('label'=>'Montos', 'content'=>
-                        $form->textFieldRow($model,'TOTAL_MERCADERIA',array('size'=>28,'maxlength'=>28))
-                        .$form->textFieldRow($model,'MONTO_ANTICIPO',array('size'=>28,'maxlength'=>28))
-                        .$form->textFieldRow($model,'MONTO_FLETE',array('size'=>28,'maxlength'=>28))
-                        .$form->textFieldRow($model,'MONTO_SEGURO',array('size'=>28,'maxlength'=>28))
-                        .$form->textFieldRow($model,'MONTO_DESCUENTO1',array('size'=>28,'maxlength'=>28))
-                        .$form->textFieldRow($model,'TOTAL_IMPUESTO1',array('size'=>28,'maxlength'=>28))
+                        $form->textFieldRow($model,'TOTAL_MERCADERIA',array('size'=>28,'maxlength'=>28,'value'=>0))
+                        .$form->textFieldRow($model,'MONTO_DESCUENTO1',array('size'=>28,'maxlength'=>28,'value'=>0))
+                        .$form->textFieldRow($model,'TOTAL_IMPUESTO1',array('size'=>28,'maxlength'=>28,'value'=>0))
                         .$form->textFieldRow($model,'TOTAL_A_FACTURAR',array('size'=>28,'maxlength'=>28,'value'=>0))
-                        .$form->textFieldRow($model,'REMITIDO',array('size'=>1,'maxlength'=>1))
+                        .$form->textFieldRow($model,'MONTO_ANTICIPO',array('size'=>28,'maxlength'=>28,'value'=>0))
+                        .$form->textFieldRow($model,'MONTO_FLETE',array('size'=>28,'maxlength'=>28,'value'=>0))
+                        .$form->textFieldRow($model,'MONTO_SEGURO',array('size'=>28,'maxlength'=>28,'value'=>0))
                         ),
                 )
             )); ?>
@@ -346,91 +395,7 @@ function cargaGrilla(grid_id){
  
 <?php $this->endWidget(); ?>
 
-    <?php 
-    $this->beginWidget('bootstrap.widgets.BootModal', array('id'=>'bodega')); ?>
- 
-	<div class="modal-body">
-                <a class="close" data-dismiss="modal">&times;</a>
-                <br>
-          <?php 
-            $this->widget('bootstrap.widgets.BootGridView', array(
-            'type'=>'striped bordered condensed',
-            'id'=>'bodega-grid',
-            'template'=>"{items} {pager}",
-            'dataProvider'=>$bodega->search(),
-            'selectionChanged'=>'cargaGrilla',
-            'filter'=>$bodega,
-            'columns'=>array(
-                array(  'name'=>'ID',
-                        'header'=>'Codigo',
-                        'htmlOptions'=>array('data-dismiss'=>'modal'),
-                        'type'=>'raw',
-                        'value'=>'CHtml::link($data->ID,"#")'
-                    ),
-                    'DESCRIPCION',
-                    'TELEFONO',
-                    array(
-                            'class'=>'bootstrap.widgets.BootButtonColumn',
-                            'htmlOptions'=>array('style'=>'width: 50px'),
-                            'template'=>'',
-                    ),
-            ),
-    ));
-      ?>
-	</div>
-        <div class="modal-footer">
-
-            <?php $this->widget('bootstrap.widgets.BootButton', array(
-                'label'=>'Cerrar',
-                'url'=>'#',
-                'htmlOptions'=>array('data-dismiss'=>'modal'),
-            )); ?>
-        </div>
- 
-<?php $this->endWidget(); ?>
-
-    <?php 
-    $this->beginWidget('bootstrap.widgets.BootModal', array('id'=>'condicion')); ?>
- 
-	<div class="modal-body">
-                <a class="close" data-dismiss="modal">&times;</a>
-                <br>
-          <?php 
-            $this->widget('bootstrap.widgets.BootGridView', array(
-            'type'=>'striped bordered condensed',
-            'id'=>'condicion-grid',
-            'template'=>"{items} {pager}",
-            'dataProvider'=>$condicion->search(),
-            'selectionChanged'=>'cargaGrilla',
-            'filter'=>$condicion,
-            'columns'=>array(
-                array(  'name'=>'ID',
-                        'header'=>'Codigo',
-                        'htmlOptions'=>array('data-dismiss'=>'modal'),
-                        'type'=>'raw',
-                        'value'=>'CHtml::link($data->ID,"#")'
-                    ),
-                    'DESCRIPCION',
-                    'DIAS_NETO',
-                    array(
-                            'class'=>'bootstrap.widgets.BootButtonColumn',
-                            'htmlOptions'=>array('style'=>'width: 50px'),
-                            'template'=>'',
-                    ),
-            ),
-    ));
-      ?>
-	</div>
-        <div class="modal-footer">
-
-            <?php $this->widget('bootstrap.widgets.BootButton', array(
-                'label'=>'Cerrar',
-                'url'=>'#',
-                'htmlOptions'=>array('data-dismiss'=>'modal'),
-            )); ?>
-        </div>
- 
-<?php $this->endWidget(); ?>
+    
 
 
 <?php $this->beginWidget('bootstrap.widgets.BootModal', array('id'=>'nuevo')); ?>
