2de85f2e385600ca928c0d430cf3a12efcd84ee4
David Perez <david.perez@tramasoft.com>
2013-02-13 15:03:46 -0500

    ??

--- protected/views/retencion/create.php
+++
@@ -1,9 +0,0 @@
-<?php
-$this->breadcrumbs=array(
-        'Sistema'=>array('create'),
-	"Retenciones");
-?>
-
-<h1>Crear Retencion</h1>
-
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
